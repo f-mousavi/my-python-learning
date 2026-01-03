41a4bd1af1bfe6bebd89687d6285c64e861b5686
fatemeh-mousavi <seydemousavi@gmail.com>
2026-01-03 17:12:36 +0330

    Add files via upload

---
+++ name_master.py
@@ -0,0 +1,8 @@
+name=input("name khod ra vared konid:")
+famili=input("famili shoma chist?")
+x=name+" "+famili
+print(x)
+if "python" in x.lower():
+    print (x.index("python"))
+else:
+    print(x , x, x)
